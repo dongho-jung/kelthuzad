c40e794df6b92d0a785f0629113f5b0aac22defc
donghojung <andrew@buzzni.com>
2019-05-15 11:40:23 +0900

    update kelthuzad.go

--- kelthuzad.go
+++ kelthuzad.go
@@ -14,8 +14,6 @@ import (
 	"time"
 )
 
-var logger *log.Logger
-
 // Kelthuzad monitors a log or stdout, kills a sick one and respawns a normal one.
 type Kelthuzad struct {
 	cmd    *exec.Cmd
@@ -26,18 +24,19 @@ type Kelthuzad struct {
 
 // opts have several options for argument parsing.
 type opts struct {
-	LogPath string `short:"p" long:"path" description:"The path of the log"`
-	CmdPath string `short:"c" long:"command" description:"The path of a command string to respawn the process" required:"true"`
-	Regex   string `short:"r" long:"regex" description:"The regex pattern to detect a failure" required:"true"`
-	Verbose bool   `short:"v" long:"verbose" description:"Print a verbose message to stdout"`
-	Delay   int    `short:"d" long:"delay" description:"The seconds for waiting after respawning" default:"5"`
+	LogPath    string `short:"l" long:"logPath" description:"The path of the log instead of stdout"`
+	CmdPath    string `short:"c" long:"commandPath" description:"The path of a file containing command string to respawn the process"`
+	RawCommand string `short:"r" long:"rawCommand" description:"The command string to spawn the process"`
+	Pattern    string `short:"p" long:"pattern" description:"The regex pattern to detect a failure" required:"true"`
+	Quiet      bool   `short:"q" long:"quiet" description:"Suppress the ouputs of process which is monitored"`
+	Delay      int    `short:"d" long:"delay" description:"The seconds for waiting after respawning" default:"5"`
 }
 
 // New returns initialized Kelthuzad pointer
 func New(opt *opts) *Kelthuzad {
 	kel := &Kelthuzad{}
 	kel.opt = opt
-	kel.regex = regexp.MustCompile(kel.opt.Regex)
+	kel.regex = regexp.MustCompile(kel.opt.Pattern)
 	kel.spawn()
 
 	return kel
@@ -45,14 +44,20 @@ func New(opt *opts) *Kelthuzad {
 
 // spawn executes the command from k.opt.CmdPath and assigns it into k's cmd field.
 func (k *Kelthuzad) spawn() {
-	cmd := exec.Command(k.opt.CmdPath)
+	var cmd *exec.Cmd
+	if k.opt.CmdPath != "" {
+		cmd = exec.Command(k.opt.CmdPath)
+	} else {
+		cmd = exec.Command("bash", "-lc", k.opt.RawCommand+" 2>&1")
+	}
 
 	if k.opt.LogPath == "" {
 		// get the stdout pipe before it starts and assign it into k.stdout to monitor stdout
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
-			logger.Fatal(err)
+			log.FATALln("[FATAL] k.spawn stdout", err)
 		}
+
 		k.stdout = stdout
 	}
 
@@ -60,12 +65,12 @@ func (k *Kelthuzad) spawn() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	go func() {
 		err := cmd.Start()
+		log.Printf("[SYSTEM] %v is spawned\n", cmd.Process.Pid)
 		if err != nil {
-			logger.Fatalln(err)
+			log.FATALln("[FATAL] k.spawn Start", err)
 		}
-		logger.Printf("%v is spawned\n", cmd.Process.Pid)
 		cmd.Wait()
-		logger.Printf("%v is done!\n", cmd.Process.Pid)
+		log.Printf("[SYSTEM] %v is done!\n", cmd.Process.Pid)
 	}()
 
 	// return the created Cmd struct
@@ -78,7 +83,7 @@ func (k *Kelthuzad) kill() {
 	if err == nil {
 		syscall.Kill(-pgid, 15)
 	} else {
-		logger.Fatal(err)
+		log.Println("[SYSTEM] the proecss was alreday terminated", err)
 	}
 }
 
@@ -87,10 +92,10 @@ func (k *Kelthuzad) check(line string) {
 	// if the line contains the pattern of k.regex
 	if k.regex.MatchString(line) {
 		// notify it
-		logger.Printf("[FAIL] %v -> %v\n", line, k.opt.Regex)
+		log.Printf("[FAIL] %v -> %v\n", line, k.opt.Pattern)
 
 		// wait to avoid being with flooded with respawning
-		logger.Printf("Waiting %v seconds...\n", k.opt.Delay)
+		log.Printf("[SYSTEM] Waiting %v seconds...\n", k.opt.Delay)
 		time.Sleep(time.Second * time.Duration(k.opt.Delay))
 
 		// kill the sick one
@@ -99,9 +104,9 @@ func (k *Kelthuzad) check(line string) {
 		// respawn the normal one
 		k.spawn()
 
-		// if the Verbose flag is set, also print normal lines
-	} else if k.opt.Verbose {
-		logger.Println(line)
+		// if the Quiet flag isn't set, also print normal lines
+	} else if k.opt.Quiet == false {
+		log.Println(line)
 	}
 }
 
@@ -110,7 +115,7 @@ func (k *Kelthuzad) monitorLog() {
 	// get the Tail struct for monitoring the last part of the log
 	t, err := tail.TailFile(k.opt.LogPath, tail.Config{Follow: true, Location: &tail.SeekInfo{Offset: 0, Whence: os.SEEK_END}})
 	if err != nil {
-		logger.Fatalln(err)
+		log.FATALln("[FATAL] k.monitorLog tail", err)
 	}
 
 	// monitor the log
@@ -132,10 +137,10 @@ func (k *Kelthuzad) monitorStdout() {
 // Monitor monitors appropriate one depending on LogPath option.
 func (k *Kelthuzad) Monitor() {
 	if k.opt.LogPath != "" {
-		logger.Println("monitoring log...")
+		log.Println("[SYSTEM] monitoring log...")
 		k.monitorLog()
 	} else {
-		logger.Println("monitoring stdout...")
+		log.Println("[SYSTEM] monitoring stdout...")
 		k.monitorStdout()
 	}
 }
@@ -144,13 +149,17 @@ func main() {
 	// initialize empty options
 	opt := &opts{}
 
-	// set the logger
-	logger = log.New(os.Stdout, "", log.LstdFlags|log.Ltime)
+	// set the log
+	log.SetFlags(log.Ltime | log.LstdFlags)
 
 	// parse the arguments
 	_, err := flags.Parse(opt)
 	if err != nil {
 		os.Exit(1)
+	}
+
+	if (opt.CmdPath == "") == (opt.RawCommand == "") {
+		log.FATALln("[FATAL] You must specify one of CmdPath, RawCommand at least!")
 	}
 
 	// get a kelthuzad object
@@ -161,7 +170,7 @@ func main() {
 	signal.Notify(signalChan, os.Interrupt)
 	go func() {
 		<-signalChan
-		logger.Println("recieved an interrupt, stopping...\n")
+		log.Println("[SYSTEM] recieved an interrupt, stopping...\n")
 		kel.kill()
 		os.Exit(0)
 	}()
